fd9e07614e3b043a95775a8d2d9de39f1a09ed6a
Matt Hoffman <mwh30@cam.ac.uk>
2015-01-30 15:12:28 +0000

    Update to include an object for parameters.

--- models/params.py
+++ models/params.py
@@ -11,43 +11,108 @@ import numpy as np
 __all__ = []
 
 
-class Parameterized(object):
-    def __init__(self):
-        self.nhyper = 0
-        self.__names = []
-        self.__hypers = []
-        self.__transforms = []
-        self.__priors = []
+class Parameter(object):
+    """
+    Representation of a parameter vector.
+    """
+    def __init__(self, value, prior=None, transform=None):
+        self.value = np.array(value, dtype=float)
+        self.size = self.value.size
+        self.prior = prior
+        self.transform = transform
 
     def __repr__(self):
-        substrings = []
-        for name, hyper in zip(self.__names, self.__hypers):
-            value = np.array2string(hyper, separator=',')
-            substrings.append('{:s}={:s}'.format(name, value))
-        return self.__class__.__name__ + '(' + ', '.join(substrings) + ')'
+        return np.array2string(self.value, separator=',')
 
-    def _link_hyper(self, name, hyper, transform=None, prior=None):
-        # FIXME: check C-order, etc?
-        # FIXME: add properties of some sort?
-        if name in self.__names:
-            raise ValueError('hyperparameter names must be unique')
-        self.nhyper += hyper.size
-        self.__names.append(name)
-        self.__hypers.append(hyper)
-        self.__transforms.append(transform)
-        self.__priors.append(prior)
+    # NOTE: for now I will assume that any priors are not themselves
+    # parameterized. Adding this would require the get/set functions to modify
+    # the prior parameters as well and logp would have to take this into
+    # account if gradients are asked for.
 
-    def set_hyper(self, hyper):
-        hyper = np.array(hyper, dtype=float, copy=False, ndmin=1)
-        if hyper.shape != (self.nhyper,):
-            raise ValueError('incorrect number of hyperparameters')
-        offset = 0
-        for hyper_ in self.__hypers:
-            hyper_.flat[:] = hyper[offset:offset+hyper_.size]
-            offset += hyper_.size
+    def get_params(self):
+        """
+        Return the parameters.
+        """
+        return self.value.copy()
 
-    def get_hyper(self):
-        if self.nhyper == 0:
+    def set_params(self, hyper):
+        """
+        Set the parameters.
+        """
+        self.value.flat[:] = hyper
+
+    def logp(self, grad=False):
+        """
+        Return the log probability of parameter assignments for this parameter
+        vector. Also if requested return the gradient of this probability with
+        respect to the parameter values.
+        """
+        if self.prior is None:
+            return (0.0, np.zeros_like(self.value.ravel())) if grad else 0.0
+        else:
+            return self.prior.logp(self.value.ravel(), grad)
+
+
+class Parameterized(object):
+    """
+    Base class for parameterized objects.
+    """
+    def __init__(self):
+        self.__params = []
+
+    def __repr__(self):
+        return (self.__class__.__name__ + '(' +
+                ', '.join('{:s}={:s}'.format(name, obj)
+                          for (name, obj) in self.__params) + ')')
+
+    @property
+    def size(self):
+        """
+        Return the number of parameters for this object.
+        """
+        return sum(obj.size for (_, obj) in self.__params)
+
+    def _register(self, name, value):
+        """
+        Register a parameter.
+        """
+        param = Parameter(value)
+        self.__params.append((name, param))
+        self.__setattr__('_' + name, param.value)
+
+    def get_params(self):
+        """
+        Return a flattened vector consisting of the parameters for the object.
+        """
+        if len(self.__params) == 0:
             return np.array([])
         else:
-            return np.hstack(_ for _ in self.__hypers)
+            return np.hstack(obj.get_params() for (_, obj) in self.__params)
+
+    def set_params(self, theta):
+        """
+        Given a parameter vector of the appropriate size, assign the values of
+        this vector to the internal parameters.
+        """
+        theta = np.array(theta, dtype=float, copy=False, ndmin=1)
+        if theta.shape != (self.size,):
+            raise ValueError('incorrect number of parameters')
+        offset = 0
+        for _, obj in self.__params:
+            obj.set_params(theta[offset:offset+obj.size])
+
+    def logp(self, grad=False):
+        """
+        Return the log probability of parameter assignments to a parameterized
+        object. Also if requested return the gradient of this probability with
+        respect to the parameter values.
+        """
+        if grad is False:
+            return sum((obj.logp(False) for (_, obj) in self.__params), 0.0)
+        else:
+            logp, dlogp = 0.0, []
+            for (_, obj) in self.__params:
+                elem = obj.logp(True)
+                logp += elem[0]
+                dlogp.append(elem[1])
+            return logp, (np.hstack(dlogp) if len(dlogp) > 0 else np.array([]))
