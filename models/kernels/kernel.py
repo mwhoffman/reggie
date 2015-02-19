8a4cd2eb4f0a651e023b34b9854343fe239e4f29
Matt Hoffman <mwh30@cam.ac.uk>
2015-02-19 14:31:52 +0000

    Fix kernel grad tests.

--- models/kernels/kernel.py
+++ models/kernels/kernel.py
@@ -6,6 +6,8 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+import numpy as np
+
 from ..core.params import Parameterized
 
 __all__ = ['Kernel']
@@ -17,6 +19,11 @@ class Kernel(Parameterized):
     """
     The base Kernel interface.
     """
+    def __call__(self, x1, x2):
+        X1 = np.array(x1, ndmin=1)[None]
+        X2 = np.array(x2, ndmin=1)[None]
+        return self.get_kernel(X1, X2)[0]
+
     def get_kernel(self, X1, X2):
         raise NotImplementedError
 
--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -10,6 +10,7 @@ from __future__ import print_function
 
 import numpy as np
 import numpy.testing as nt
+import scipy.optimize as spop
 
 import models.kernels as mk
 
@@ -33,11 +34,35 @@ class KernelTest(object):
                                                                  self.X1)])
         nt.assert_allclose(g, G)
 
+    def test_call(self):
+        m = self.X1.shape[0]
+        n = self.X2.shape[0]
+        K = self.kernel.get_kernel(self.X1, self.X2)
+        K_ = np.array([self.kernel(x1, x2)
+                       for x1 in self.X1
+                       for x2 in self.X2]).reshape(m, n)
+        nt.assert_equal(K, K_)
+
+    def test_grad(self):
+        x = self.kernel.get_params()
+        k = lambda x, x1, x2: self.kernel.copy(x)(x1, x2)
+
+        G = np.array(list(self.kernel.get_grad(self.X1, self.X2)))
+        m = self.X1.shape[0]
+        n = self.X2.shape[0]
+
+        G_ = np.array([spop.approx_fprime(x, k, 1e-8, x1, x2)
+                       for x1 in self.X1
+                       for x2 in self.X2]).swapaxes(0, 1).reshape(-1, m, n)
+
+        nt.assert_allclose(G, G_, rtol=1e-6, atol=1e-6)
+
 
 class RealKernelTest(KernelTest):
     def __init__(self, kernel):
-        X1 = np.random.rand(5, kernel._ndim)
-        X2 = np.random.rand(5, kernel._ndim)
+        rng = np.random.RandomState(0)
+        X1 = rng.rand(5, kernel._ndim)
+        X2 = rng.rand(3, kernel._ndim)
         super(RealKernelTest, self).__init__(kernel, X1, X2)
 
 
