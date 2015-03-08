1a133e0e5e533acdd62492fd75868f0eed51a50b
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-08 22:25:15 +0000

    Update plotting.

--- models/plotting.py
+++ models/plotting.py
@@ -9,13 +9,10 @@ from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as pl
 
-from .core.models import PosteriorModel
-
 __all__ = ['plot_posterior']
 
 
 def _figure(fig, draw=True):
-    fig.tight_layout()
     if draw:
         fig.canvas.draw()
     return fig
@@ -31,14 +28,15 @@ def _axis(ax, despine=True, draw=True):
     return ax
 
 
-def plot_posterior(model, xmin=None, xmax=None, data=True, **kwargs):
+def plot_posterior(model, xmin=None, xmax=None, data=True, predictive=False,
+                   **kwargs):
     """
     Plot the marginal distribution of the given one-dimensional posterior
     model.
     """
 
-    if not isinstance(model, PosteriorModel):
-        raise ValueError('model must be a PosteriorModel instance')
+    # if not isinstance(model, PosteriorModel):
+    #     raise ValueError('model must be a PosteriorModel instance')
 
     if model.ndata == 0 and (xmin is None or xmax is None):
         raise ValueError('model has no data and no bounds are given')
@@ -52,7 +50,7 @@ def plot_posterior(model, xmin=None, xmax=None, data=True, **kwargs):
     x = np.linspace(xmin, xmax, 500)
 
     # get the posterior mean and confidence bands
-    mu, s2 = model.get_posterior(x[:, None])
+    mu, s2 = model.get_posterior(x[:, None], predictive=predictive)
     lo = mu - 2 * np.sqrt(s2)
     hi = mu + 2 * np.sqrt(s2)
 
@@ -100,6 +98,37 @@ def plot_chain(samples, names=None, **kwargs):
         if i < d-1:
             ax.set_xticklabels([])
         ax = _axis(ax, draw=False, **kwargs)
+
+    # complete the figure
+    _figure(fig, draw)
+
+
+def plot_pairs(samples, names=None, **kwargs):
+    fig = pl.gcf()
+    fig.clf()
+    draw = kwargs.pop('draw', True)
+
+    d = samples.shape[1]
+
+    for i, j in np.ndindex(d, d):
+        if i >= j:
+            continue
+        ax = fig.add_subplot(d-1, d-1, (j-1)*(d-1)+i+1)
+        ax.scatter(samples[:, i], samples[:, j], edgecolor='white', alpha=0.1)
+
+        # get rid of the internal labels
+        if i > 0:
+            ax.set_yticklabels([])
+        if j < d-1:
+            ax.set_xticklabels([])
+
+        # set the labels
+        if i == 0 and names is not None:
+            ax.set_ylabel(names[j])
+        if j == d-1 and names is not None:
+            ax.set_xlabel(names[i])
+
+        _axis(ax, draw=False, **kwargs)
 
     # complete the figure
     _figure(fig, draw)
