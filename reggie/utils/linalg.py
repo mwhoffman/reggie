3f4a979bbb1e672256535bcaab1bfad8f53f983b
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-12 18:32:35 +0000

    Add linalg tests.

--- reggie/utils/linalg.py
+++ reggie/utils/linalg.py
@@ -13,10 +13,15 @@ from scipy.linalg import LinAlgError
 from numpy.lib.stride_tricks import as_strided
 
 
-__all__ = []
+__all__ = ['add_diagonal', 'cholesky', 'cholesky_inverse', 'solve_triangular',
+           'solve_cholesky']
 
 
 def add_diagonal(A, d, copy=False):
+    """
+    Return the matrix `A` where its diagonal has had the vector `d` added to
+    it. If `copy` is False perform the addition in place.
+    """
     if copy:
         A = A.copy()
     shape = (A.shape[0],)
@@ -27,6 +32,11 @@ def add_diagonal(A, d, copy=False):
 
 
 def cholesky(A, maxtries=5):
+    """
+    Compute the cholesky of A. If the matrix is singular make `maxtries`
+    additional attempts to invert it by adding small amounts to the diagonal
+    before giving up.
+    """
     L, info = lapack.dpotrf(A, lower=1)
     if info == 0:
         return L
@@ -36,6 +46,7 @@ def cholesky(A, maxtries=5):
             raise LinAlgError('Matrix has non-positive diagonal elements')
         j = d.mean() * 1e-6
         for _ in xrange(maxtries):
+            # FIXME: we should still raise a warning here.
             L, info = lapack.dpotrf(add_diagonal(A, j, True), lower=1)
             if info == 0:
                 return L
@@ -45,6 +56,9 @@ def cholesky(A, maxtries=5):
 
 
 def cholesky_inverse(A):
+    """
+    Compute the inverse of the matrix AA' given its cholesky decomposition A.
+    """
     X, info = lapack.dpotri(A, lower=1)
     if info != 0:
         raise LinAlgError('Matrix is not invertible')
@@ -54,6 +68,10 @@ def cholesky_inverse(A):
 
 
 def solve_triangular(A, B, trans=0):
+    """
+    Solve the system Ax=B where A is lower triangular. If `trans` is 1 then
+    solve the system A'x=B.
+    """
     X, info = lapack.dtrtrs(A, B, lower=1, trans=trans)
     if info != 0:
         raise LinAlgError('Matrix is singular')
@@ -61,5 +79,9 @@ def solve_triangular(A, B, trans=0):
 
 
 def solve_cholesky(A, B):
+    """
+    Solve the system Mx=B where A is the lower-triangular cholesky
+    decomposition of the matrix M.
+    """
     X, _ = lapack.dpotrs(A, B, lower=1)
     return X
