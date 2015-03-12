7670418d337dc7f6209bc7226adc4669c9a8f1d5
Matt Hoffman <mwh30@cam.ac.uk>
2015-03-12 19:00:56 +0000

    Update kernel tests.

--- tests/test_kernels.py
+++ tests/test_kernels.py
@@ -23,16 +23,8 @@ class KernelTest(object):
         self.X1 = X1
         self.X2 = X2
 
-    def test_kernel(self):
-        K = self.kernel.get_kernel(self.X1, self.X1)
-        k = self.kernel.get_dkernel(self.X1)
-        nt.assert_allclose(k, K.diagonal())
-
-    def test_dgrad(self):
-        g = np.array(list(self.kernel.get_dgrad(self.X1)))
-        G = np.array([_.diagonal() for _ in self.kernel.get_grad(self.X1,
-                                                                 self.X1)])
-        nt.assert_allclose(g, G)
+    def test_repr(self):
+        _ = repr(self.kernel)
 
     def test_call(self):
         m = self.X1.shape[0]
@@ -43,7 +35,18 @@ class KernelTest(object):
                        for x2 in self.X2]).reshape(m, n)
         nt.assert_equal(K, K_)
 
-    def test_grad(self):
+    def test_get_kernel(self):
+        K = self.kernel.get_kernel(self.X1, self.X1)
+        k = self.kernel.get_dkernel(self.X1)
+        nt.assert_allclose(k, K.diagonal())
+
+    def test_get_dgrad(self):
+        g = np.array(list(self.kernel.get_dgrad(self.X1)))
+        G = np.array([_.diagonal() for _ in self.kernel.get_grad(self.X1,
+                                                                 self.X1)])
+        nt.assert_allclose(g, G)
+
+    def test_get_grad(self):
         x = self.kernel.get_params()
         k = lambda x, x1, x2: self.kernel.copy(x)(x1, x2)
 
@@ -64,6 +67,19 @@ class RealKernelTest(KernelTest):
         X1 = rng.rand(5, kernel.ndim)
         X2 = rng.rand(3, kernel.ndim)
         super(RealKernelTest, self).__init__(kernel, X1, X2)
+
+    def test_get_gradx(self):
+        G1 = self.kernel.get_gradx(self.X1, self.X2)
+        m = self.X1.shape[0]
+        n = self.X2.shape[0]
+        d = self.X1.shape[1]
+        k = self.kernel
+
+        G2 = np.array([spop.approx_fprime(x1, k, 1e-8, x2)
+                       for x1 in self.X1
+                       for x2 in self.X2]).reshape(m, n, d)
+
+        nt.assert_allclose(G1, G2, rtol=1e-6, atol=1e-6)
 
 
 ### PER-INSTANCE TEST CLASSES #################################################
