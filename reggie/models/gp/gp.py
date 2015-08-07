Fix FITC input gradients.

reggie/models/gp/gp.py
@@ -66,7 +66,7 @@ class GP(ParameterizedModel):
         # append the inference method if it is non-default
         if inf in inference.__all__:
             if inf is not 'exact':
-                info.append(('inf', inference))
+                info.append(('inf', inf))
         else:
             info.append(('inf', self._infer))
 
@@ -120,13 +120,12 @@ class GP(ParameterizedModel):
 
         if self._post is not None:
             # get the kernel gradients
-            if hasattr(self._post, 'U'):
-                dK = self._kern.get_gradx(X, self._post.U)
+            if self._U is not None:
+                dK = self._kern.get_gradx(X, self._U)
             else:
                 dK = self._kern.get_gradx(X, self._X)
 
             # reshape them to make it a 2d-array
-            dK = self._kern.get_gradx(X, self._X)
             dK = np.rollaxis(dK, 1)
             dK = np.reshape(dK, (dK.shape[0], -1))
 

tests/test_models.py
@@ -34,7 +34,6 @@ class ModelTest(object):
         model.add_data(X[n:], Y[n:])
         nt.assert_allclose(model.predict(X), self.model.predict(X))
 
-    """
     def test_get_loglike(self):
         # first make sure we can call it with zero data.
         _ = self.model.copy(reset=True).get_loglike()
@@ -67,7 +66,6 @@ class ModelTest(object):
         G1 = self.model.predict(X, grad=True)[3]
         G2 = np.array([spop.approx_fprime(x, f, 1e-8) for x in X])
         nt.assert_allclose(G1, G2, rtol=1e-6, atol=1e-6)
-    """
 
 
 ### PER-INSTANCE TEST CLASSES #################################################
@@ -82,13 +80,13 @@ class TestGP(ModelTest):
 class TestGP_FITC(ModelTest):
     def __init__(self):
         U = np.random.rand(50, 2)
-        gp = models.make_gp(0.7, 1, [1., 1.], inference='fitc', U=U)
+        gp = models.make_gp(0.7, 1, [1., 1.], inf='fitc', U=U)
         gp.add_data(np.random.rand(10, 2), np.random.rand(10))
         ModelTest.__init__(self, gp)
 
 
 class TestGP_Laplace(ModelTest):
     def __init__(self):
-        gp = models.make_gp(0.7, 1, [1., 1.], inference='laplace')
+        gp = models.make_gp(0.7, 1, [1., 1.], inf='laplace')
         gp.add_data(np.random.rand(10, 2), np.random.rand(10))
         ModelTest.__init__(self, gp)